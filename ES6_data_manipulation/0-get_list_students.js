3fe69d15802a818376202bc9d0cb683dc2ad64eb
MatthieuGrauleau <m.grauleau@gmail.com>
2024-07-22 09:42:12 +0200

    chaging name

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
 export default function getListStudents() {
-  const listStudents = [
+  const listofstudents = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
-  return listStudents.map((student) => ({
+  return listofstudents.map((student) => ({
     id: student.id,
     firstName: student.firstName,
     location: student.location,
