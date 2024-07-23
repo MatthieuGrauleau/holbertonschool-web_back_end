fc8248033a5ad75de5ad8bdb8e6f77294666d0c4
MatthieuGrauleau <m.grauleau@gmail.com>
2024-07-23 10:39:39 +0200

    fix eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,20 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const userpromise = signUpUser(firstName, lastName);
-  const photopromise = uploadPhoto(fileName);
-  const results = await Promise.allSettled([userpromise, photopromise]);
-
-  return results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: 'fulfilled',
-        value: result.value,
-      };
-    }
-    return {
-      status: 'rejected',
-      value: result.reason,
-    };
-  });
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return result.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+  }));
 }
