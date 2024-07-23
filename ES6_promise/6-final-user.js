94dba602080d2fc067d3a66ea12845162e05d80b
MatthieuGrauleau <m.grauleau@gmail.com>
2024-07-23 10:37:39 +0200

    changing

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,18 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: 'fulfilled',
-          value: result.value,
-        };
-      }
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userpromise = signUpUser(firstName, lastName);
+  const photopromise = uploadPhoto(fileName);
+  const results = await Promise.allSettled([userpromise, photopromise]);
+
+  return results.map((result) => {
+    if (result.status === 'fulfilled') {
       return {
-        status: 'rejected',
-        value: result.reason,
+        status: 'fulfilled',
+        value: result.value,
       };
-    }));
+    }
+    return {
+      status: 'rejected',
+      value: result.reason,
+    };
+  });
 }
