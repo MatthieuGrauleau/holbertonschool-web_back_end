d3273b316922ea6edd71043e66984115d745b585
MatthieuGrauleau <m.grauleau@gmail.com>
2024-08-16 15:15:30 +0200

    simplif

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -12,11 +12,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     """he code is nearly identical to wait_n
     except task_wait_random is being called."""
-    tasks = [task_wait_random(max_delay) for i in range(0, n)]
-
-    waiting = []
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        insort(waiting, delay)
-
-    return waiting
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
